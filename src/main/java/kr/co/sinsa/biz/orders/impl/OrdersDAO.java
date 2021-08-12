fa4930da6ff5ef75aa1ad87f683a0a79bb8b4df4
Da Yeon Han <spaky1637@gmail.com>
2021-08-12 15:31:26 +0900

    Update OrdersDAO

--- src/main/java/kr/co/sinsa/biz/orders/impl/OrdersDAO.java
+++ src/main/java/kr/co/sinsa/biz/orders/impl/OrdersDAO.java
@@ -1,5 +1,11 @@
 package kr.co.sinsa.biz.orders.impl;
 
-public class OrdersDAO {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class OrdersDAO {
+	@Autowired
+	private SqlSessionTemplate SST;
 }
