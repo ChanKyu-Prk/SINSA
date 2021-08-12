e9ae79a6ea3f6b20df72f6d3ba866517967f8fba
Da Yeon Han <spaky1637@gmail.com>
2021-08-12 11:32:43 +0900

    Add orders-related files

---
+++ src/main/java/kr/co/sinsa/view/orders/OrdersController.java
@@ -0,0 +1,14 @@
+package kr.co.sinsa.view.orders;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import kr.co.sinsa.biz.orders.OrdersService;
+
+@Controller
+public class OrdersController {
+	@Autowired
+	private OrdersService service;
+	
+
+}
