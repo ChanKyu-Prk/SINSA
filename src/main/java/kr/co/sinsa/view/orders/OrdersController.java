eadb75259811ecc27764d2dbfbee37fd7fde3094
Da Yeon Han <spaky1637@gmail.com>
2021-08-19 21:06:45 +0900

    Update OrdersController.java

--- src/main/java/kr/co/sinsa/view/orders/OrdersController.java
+++ src/main/java/kr/co/sinsa/view/orders/OrdersController.java
@@ -1,14 +1,10 @@
 package kr.co.sinsa.view.orders;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +13,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import kr.co.sinsa.biz.customer.CustomerVO;
+import kr.co.sinsa.biz.orders.OrdersAndProductVO;
 import kr.co.sinsa.biz.orders.OrdersSerivce;
-import kr.co.sinsa.biz.orders.OrdersVO;
 import kr.co.sinsa.biz.user.UserVO; 	
 
 
 @Controller
+@SessionAttributes("prdInfo")
 public class OrdersController {
 	@Autowired
 	private OrdersSerivce service;
@@ -44,27 +41,37 @@ public class OrdersController {
 			CustomerVO vo = service.cusInfoView(CUS_ID);
 			model.addAttribute("cusInfo", vo);
 		}
-		
+		System.out.println("GET이다");
 		return "/orders/checkout";
 	}
 	
 	// 
 	@RequestMapping(value = "/direct/checkout", method=RequestMethod.POST,produces = "application/json;charset=UTF-8")
-    public String details(@RequestBody List<Map<String, String>> itemLists, Model model) {
+    public String details(@RequestBody List<Map<String, String>> itemLists, Model model) throws Exception {
+		String ORDER_PRDCODE = null;
+		String ORDER_PRDSIZE = null;
+		String ORDER_AMOUNT = null;
+		OrdersAndProductVO oapVO = null;
+		List<OrdersAndProductVO> prdList = new ArrayList();
+				
 		int index = 0;
+		
 		for(Object list : itemLists) {
 //			System.out.println(list);
 			LinkedHashMap<String,String> item = (LinkedHashMap<String, String>) list;
+			System.out.println("index : " + index);
+			System.out.println(item.get("ORDER_PRDCODE"));
+			oapVO = service.selPrdByCode(item.get("ORDER_PRDCODE"));
+			prdList.add(oapVO);
+			
+			System.out.println(item.get("ORDER_PRDSIZE"));
+			System.out.println(item.get("ORDER_AMOUNT"));
 			index++;
-			for (Entry<String, String> entry : item.entrySet()) {
-			   String key = entry.getKey();
-			   Object value = entry.getValue();
-			   System.out.println(key+"["+index+"] : "+value + " / entry : "+ entry); 
-			   				   // ORDER_PRDSIZE[1] : 230         / entry : ORDER_PRDSIZE=230
-			   
-			}
 		}
-//		
+		
+		System.out.println("prdList : " + prdList);
+		model.addAttribute("prdInfo", prdList);
+		System.out.println("POST이다");
 		
 //        String prdCode = orders.getORDER_PRDCODE();
 //        System.out.println("orders : " + orders.getORDER_PRDCODE());
