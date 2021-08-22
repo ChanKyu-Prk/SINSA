0e60684b0c69e1c9fa0ae35df20a025677e77413
Da Yeon Han <spaky1637@gmail.com>
2021-08-23 00:24:18 +0900

    Update Orders
    회원을 제외한 데이터 전송

--- src/main/java/kr/co/sinsa/view/orders/OrdersController.java
+++ src/main/java/kr/co/sinsa/view/orders/OrdersController.java
@@ -5,16 +5,20 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.co.sinsa.biz.customer.CustomerVO;
 import kr.co.sinsa.biz.orders.OrdersAndProductVO;
@@ -74,29 +78,34 @@ public class OrdersController {
 	}
 	
 	@RequestMapping(value = "/checkout/complete", method=RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public String payComplete(@RequestBody List<Map<String, String>> itemLists, OrdersVO ordersVO, Model model) throws Exception {
-		System.out.println("POSTjson" + itemLists);
-		System.out.println(ordersVO.getORDER_MEMO());
+    @ResponseBody
+	public String payComplete(@RequestBody List<Map<String, String>> itemLists, OrdersVO ordersVO, RedirectAttributes ra, HttpSession session, HttpServletRequest request) throws Exception {
+		System.out.println("POST");
 		OrdersAndProductVO oapVO = null;
+		List<OrdersVO> orderList = new ArrayList();
+		
 		for(Object list : itemLists) {
 			LinkedHashMap<String,String> item = (LinkedHashMap<String, String>) list;
 			oapVO = service.selPrdByCode(item.get("ORDER_PRDCODE"));
 			ordersVO.setORDER_NUM(item.get("ORDER_NUM"));
-			ordersVO.setORDER_CUSID("manbal1"); //
+			ordersVO.setORDER_CUSID("test2"); // 아이디
 			ordersVO.setORDER_PRDCODE(item.get("ORDER_PRDCODE"));
 			ordersVO.setORDER_PRDNAME(oapVO.getPRD_NAME());
 			ordersVO.setORDER_PRDSIZE(item.get("ORDER_PRDSIZE"));
 			ordersVO.setORDER_AMOUNT(Integer.parseInt(item.get("ORDER_AMOUNT")));
-			ordersVO.setORDER_RECEIVER("dhan03"); //
-			ordersVO.setORDER_TEL("dhan03"); //
-			ordersVO.setORDER_ADDR("dhan03"); //주소 합체
+			ordersVO.setORDER_RECEIVER(item.get("ORDER_RECEIVER"));
+			ordersVO.setORDER_TEL(item.get("ORDER_TEL"));
+			ordersVO.setORDER_ADDR(item.get("ORDER_ADDR"));
 			ordersVO.setORDER_PRICE(oapVO.getPRD_PRICE()*Integer.parseInt(item.get("ORDER_AMOUNT")));
-			ordersVO.setORDER_MEMO("dhan03"); //
-			ordersVO.setORDER_USEPOINT(Integer.parseInt("1000")); //
+			ordersVO.setORDER_MEMO(item.get("ORDER_MEMO"));
+			ordersVO.setORDER_USEPOINT(Integer.parseInt(item.get("ORDER_USEPOINT")));
 			service.addOrders(ordersVO);
 			//리스트로 합치고 model.addAttribute. jsp에서 사용
+			orderList.add(ordersVO);
 		}
+		
+		ra.addFlashAttribute("ordersInfo", orderList);
 
-    return "/orders/payComplete";
+		return "/orders/payComplete";
 	}
 }
