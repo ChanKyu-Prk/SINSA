6bd6dcb0c3b5792c5c2f6352a6f7da914cc12b3f
Your Name <you@example.com>
2021-09-06 21:48:19 +0900

    파일업로드주소변경

--- src/main/java/kr/co/sinsa/admin/service/PrdFileUploadService.java
+++ src/main/java/kr/co/sinsa/admin/service/PrdFileUploadService.java
@@ -2,9 +2,13 @@ package kr.co.sinsa.admin.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import kr.co.sinsa.admin.vo.PrdVO;
 
 public interface PrdFileUploadService {
 	
-public String prdImgUpload(List<MultipartFile> multipartFile);
+public String prdImgUpload(List<MultipartFile> multipartFile, HttpServletRequest req, PrdVO vo);
 }
